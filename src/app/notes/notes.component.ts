c222fbdeedada4b1773947e816fe46c8428c1c65
crisgonzalez94 <cristiannazarenogonzalez@gmail.com>
2020-07-25 21:15:28 -0300

    Added functionality for add notes in localstorage , soon i will make a database

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -24,7 +24,9 @@ export class NotesComponent implements OnInit {
   public notes: Array<Note>;
 
   constructor() { 
+
     /*For the moment , we will go add a notes of testing , below */
+    /*
     this.notes = [new Note(
                     'Things for to buy',
                     'It is been a long time since i made the purchases.',
@@ -36,7 +38,37 @@ export class NotesComponent implements OnInit {
                     ],
                     '20-11-2020'
                   )
-                  ]
+                  ];
+                  */
+
+    
+    
+
+    if(typeof(Storage) != undefined){
+
+      this.notes = JSON.parse(localStorage.getItem("angularNotes"));
+
+      /*If is a first time that the user use this app , the localstorage ,
+      will be empty , so we will make an note of example*/
+      if(this.notes == null){
+        this.notes =  [new Note(
+                                'Things for to buy',
+                                'It is been a long time since i made the purchases.',
+                                [                 
+                                  {
+                                    'work': 'Wash the dishies',
+                                    'checked': false
+                                  }
+                                ],
+                                '20-11-2020'
+                              )
+                      ];
+
+
+      }
+
+    }          
+
   }
 
   ngOnInit(): void {
@@ -57,6 +89,9 @@ export class NotesComponent implements OnInit {
 
     this.notes.push(newNote);
 
+    //Update localstorage
+    localStorage.setItem("angularNotes" , JSON.stringify(this.notes) );
+
   }
 
   //This function is for add work a note (recive as param the index of the note in array of notes)
@@ -73,17 +108,43 @@ export class NotesComponent implements OnInit {
                               );
     }
     
+    //Update localstorage
+    localStorage.setItem("angularNotes" , JSON.stringify(this.notes) );
 
   }
 
   //Function for check work of note
   checkWork(noteIndex , workNote){
     this.notes[noteIndex].works[workNote].checked = true;
+
+    //Update localstorage
+    localStorage.setItem("angularNotes" , JSON.stringify(this.notes) );
   }
 
   //Function for descheck note
   uncheckWork(noteIndex , workNote){
     this.notes[noteIndex].works[workNote].checked = false;
+
+    //Update local storage
+    localStorage.setItem("angularNotes" , JSON.stringify(this.notes) );
+  }
+
+  //Function for delete work
+  deleteWork(noteIndex , workNote){
+
+    this.notes[noteIndex].works.splice(workNote , 1);
+
+    //Update localstorage
+    localStorage.setItem("angularNotes" , JSON.stringify(this.notes) );
+  }
+
+  deleteNote(noteIndex){
+
+    this.notes.splice(noteIndex , 1);
+
+    //Update localstorage
+    localStorage.setItem("angularNotes" , JSON.stringify(this.notes) );
+
   }
 
 }
